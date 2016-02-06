6777b5b6b410641457f4a8ce878d2d45d3eef942
Maryam Muosavi <s.m.muosavi73@gmail.com>
2016-02-07 00:50:19 +0330

    login

--- MyAppointer/Controllers/UserController.cs
+++ MyAppointer/Controllers/UserController.cs
@@ -5,8 +5,13 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Transactions;
+using System.Web.Security;
+using DotNetOpenAuth.AspNet;
+using Microsoft.Web.WebPages.OAuth;
+using WebMatrix.WebData;
+using MyAppointer.Filters;
 using MyAppointer.Models;
-
 namespace MyAppointer.Controllers
 {
     public class UserController : Controller
@@ -46,7 +51,6 @@ namespace MyAppointer.Controllers
 
         //
         // POST: /User/Create
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Users users)
@@ -59,15 +63,13 @@ namespace MyAppointer.Controllers
             //}
 
             //return View(users);
-
-
             if (ModelState.IsValid)
             {
                 db.Users.Add(users);
                 db.SaveChanges();
 
 
-                if (users.Role=="jobowner")
+                if (users.Role == "jobowner")
                 {
                     //@Url.Action("ViewStockNext", "Inventory", new { firstItem = 11 });
 
@@ -91,44 +93,102 @@ namespace MyAppointer.Controllers
 
             return View(users);//jobs
         }
-        
+
         //login
         [HttpGet]
         public ActionResult Login()
         {
             return View();
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Login(Users u)
         {
-            // this action is for handle post (login)
-            if (ModelState.IsValid) // this is check validity
-            {
-                using (db)
-                {
-                    var v = db.Users.Where(a => a.Email.Equals(u.Email) && a.Password.Equals(u.Password)).FirstOrDefault();
-                    if (v != null)
-                    {
-                        Session["LogedUserID"] = v.Id.ToString();
-                        Session["LogedUserFullname"] = v.FullName.ToString();
-                        return RedirectToAction("AfterLogin");
-                    }
-                }
-            }
-            return View(u);
-        }
-        public ActionResult AfterLogin()
+        // this action is for handle post (login)
+        //if (ModelState.IsValid) // this is check validity
+        //{
+          using (db)
+               {
+                   var v = db.Users.Where(model => model.Email.Equals(u.Email) && model.Password.Equals(u.Password)).FirstOrDefault();
+                   if (v != null)
+                   {
+                       Session["LogedUserID"] = v.Id.ToString();
+                       Session["LogedUserFullname"] = v.FullName.ToString();
+                       return RedirectToAction("AfterLogin", "User");
+                   }
+                   else
+                   {
+                       return RedirectToAction("Create", "User");
+                   }
+               }
+        //}
+         }
+         public ActionResult AfterLogin()
+         {
+             if (Session["LogedUserID"] != null)
+             {
+                 return View();
+             }
+             else
+             {
+                 return RedirectToAction("Index");
+              }
+         }
+
+        // GET: /Account/Login
+
+        //[AllowAnonymous]
+        //public ActionResult Login(string returnUrl)
+        //{
+        //    ViewBag.ReturnUrl = returnUrl;
+        //   return View();
+        //}
+
+        //
+        // POST: /Account/Login
+
+        //[HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Login(Users user, string returnUrl)
+        //{
+        //  var v = db.Users.Where(model => model.Email.Equals(user.Email)).FirstOrDefault();// model.Password.Equals(u.Password)
+        //    if (v != null)
+        //    {
+        //        return RedirectToLocal(returnUrl);
+        //   }
+
+            // If we got this far, something failed, redisplay form
+        //    ModelState.AddModelError("", "The user name or password provided is incorrect.");
+        //    return View(user);
+        //}
+        //#region Helpers
+        //private ActionResult RedirectToLocal(string returnUrl)
+        //{
+        //    if (Url.IsLocalUrl(returnUrl))
+        //    {
+        //        return Redirect(returnUrl);
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("Index", "Home");
+        //    }
+            
+        //}
+
+        //#endregion
+        //
+        // POST: /Account/LogOff
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
         {
-            if (Session["LogedUserID"] != null)
-            {
-                return View();
-            }
-            else
-            {
-                return RedirectToAction("Index");
-            }
+            WebSecurity.Logout();
+            return RedirectToAction("Index", "Home");
         }
+
+
 
         public ActionResult SelectCategory()
         {

dadf48621f69270a9b25af6b06d0346d4750117c
mehdad <m.mohamadi313@gmail.com>
2016-02-09 09:51:46 +0330

    TimeTable Partial and model added

--- MyAppointer/Controllers/HomeController.cs
+++ MyAppointer/Controllers/HomeController.cs
@@ -34,6 +34,7 @@ namespace MyAppointer.Controllers
 
         public ActionResult info(int JobOwnerId)
         {
+            TimeTable timeTable = new TimeTable();
             Users user = db.Users.Find(JobOwnerId);
             Jobs job = db.Jobs.Where(model => model.FirstJobOwner.Equals(JobOwnerId)).FirstOrDefault();
             JobOwners jobowner = db.JobOwners.Where(model => model.JobId.Equals(job.Id)).FirstOrDefault();
@@ -50,25 +51,35 @@ namespace MyAppointer.Controllers
             var weeklyworkingdays = db.WeeklyWorkingDays.Where(model => model.WorkingTimesId.Equals(workingTime.Id));
 
             List<WeeklyWorkingDays> days = new List<WeeklyWorkingDays>();
+
             foreach (WeeklyWorkingDays wwd in weeklyworkingdays)
             {
                 ViewBag.day += (wwd.Day+1).ToString();
                 days.Add(wwd);
                 //ViewBag.Message += "  " + wwd.Day; 
             }
+            timeTable.days = days;
+
             ViewBag.days = days;
 
             var weeklyworkingTimes = db.WeeklyWorkingTimes.Where(model => model.WorkingTimesId.Equals(workingTime.Id));
+            List<WeeklyWorkingTimes> times = new List<WeeklyWorkingTimes>();
             foreach (WeeklyWorkingTimes wwt in weeklyworkingTimes)
             {
+                times.Add(wwt);
                 ViewBag.Times += wwt.StartTime+"-" + wwt.EndTime + ",";
             }
+            timeTable.times = times;
+
+            ViewBag.timeTable = timeTable;
 
             var offdays = db.OffDays.Where(model => model.WorkingTimesId.Equals(workingTime.Id));
             foreach (OffDays od in offdays)
             {
                 ViewBag.Message = od.OffDay;
             }
+
+
 
             //Services service = services[1];
             //if (user == null)
---
+++ MyAppointer/Models/TimeTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyAppointer.Models
+{
+    public class TimeTable
+    {
+        public List<WeeklyWorkingDays> days { get; set; }
+        public List<WeeklyWorkingTimes> times { get; set; }
+    }
+}

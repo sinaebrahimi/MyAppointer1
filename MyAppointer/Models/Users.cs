48b8f8b8bb80f8e6c061b9b0c3eaec0477fbc590
Maryam Muosavi <s.m.muosavi73@gmail.com>
2016-02-12 12:58:13 +0330

    usrs model change

--- MyAppointer/Models/Users.cs
+++ MyAppointer/Models/Users.cs
@@ -28,7 +28,7 @@ namespace MyAppointer.Models
         [EmailAddress]
         [StringLength(32)]
         [DataType(DataType.EmailAddress)]
-        //[Remote("doesUserNameExist", "User", HttpMethod = "POST", ErrorMessage = "User name already exists. Please enter a different user name.")]
+        [Remote("doesUserNameExist", "User", HttpMethod = "POST", ErrorMessage = "User name already exists. Please enter a different user name.")]
         public string Email { get; set; }
 
         [Required]
@@ -40,7 +40,7 @@ namespace MyAppointer.Models
         [Display(Name = "Mobile")]
         [Required]
         [StringLength(11)]
-        //[Remote("doesPhoneNumberExist", "User", HttpMethod = "POST", ErrorMessage = "Phone Number already exists. Please enter a different phne number.")]
+        [Remote("doesPhoneNumberExist", "User", HttpMethod = "POST", ErrorMessage = "Phone Number already exists. Please enter a different phne number.")]
         //^[01]?[- .]?(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?\d{3}[- .]?\d{4}$
         [RegularExpression(@"^09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{7}$", ErrorMessage = "Enter Your Mobile Phone Number like this: 09127404062")]
         public string Phone { get; set; }
